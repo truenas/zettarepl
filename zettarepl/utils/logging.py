d2bee14a73e05e55582bcbf5f6bc853c6e0bccf0
themylogin <themylogin@gmail.com>
2019-02-21 22:33:56 +0100

    utils.logging.LongStringsFilter

---
+++ zettarepl/utils/logging.py
@@ -0,0 +1,47 @@
+# -*- coding=utf-8 -*-
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+__all__ = ["LongStringsFilter"]
+
+
+class LongStringsFilter(logging.Filter):
+    def __init__(self, name=""):
+        super().__init__(name)
+
+        self.max_string_length = int(os.environ.get("LOGGING_MAX_STRING_LENGTH", "64"))
+
+    def filter(self, record):
+        record.args = self._process(record.args)
+        return True
+
+    def _process(self, value):
+        if isinstance(value, dict):
+            return {k: self._process(v) for k, v in value.items()}
+
+        if isinstance(value, list):
+            return list(map(self._process, value))
+
+        if isinstance(value, tuple):
+            return tuple(map(self._process, value))
+
+        if self.max_string_length:
+            if isinstance(value, bytes):
+                placeholder = b"...."
+            elif isinstance(value, str):
+                placeholder = "...."
+            else:
+                return value
+
+            if len(value) <= self.max_string_length:
+                return value
+
+            return (
+                value[:int((self.max_string_length - 4) / 2)] +
+                placeholder +
+                value[-int((self.max_string_length - 4) / 2):]
+            )
+
+        return value
