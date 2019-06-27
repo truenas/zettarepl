b55711b82af392259a81e1a05d718e3a0a078c2f
themylogin <themylogin@gmail.com>
2019-06-27 17:42:26 +0200

    Fix For task 'task_1' unhandled replication error OSError(64, 'Host is down')

--- zettarepl/replication/run.py
+++ zettarepl/replication/run.py
@@ -118,6 +118,8 @@ def run_replication_tasks(local_shell: LocalShell, transport: Transport, remote_
                     raise RecoverableReplicationError("Network connection timeout") from None
                 except paramiko.ssh_exception.NoValidConnectionsError as e:
                     raise RecoverableReplicationError(str(e).replace("[Errno None] ", "")) from None
+                except (IOError, OSError) as e:
+                    raise RecoverableReplicationError(str(e)) from None
                 replication_tasks_parts_left[replication_task.id] -= 1
                 if replication_tasks_parts_left[replication_task.id] == 0:
                     notify(observer, ReplicationTaskSuccess(replication_task.id))
