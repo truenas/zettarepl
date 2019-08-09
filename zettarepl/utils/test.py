fd1fa2697cd064f81b0813c722ebdb823579ea3b
themylogin <themylogin@gmail.com>
2019-08-09 19:26:28 +0200

    Strip SSH keys from replication tests

--- zettarepl/utils/test.py
+++ zettarepl/utils/test.py
@@ -1,5 +1,6 @@
 # -*- coding=utf-8 -*-
 import logging
+import subprocess
 import time
 from unittest.mock import PropertyMock
 
@@ -11,6 +12,15 @@ __all__ = ["mock_name", "wait_replication_tasks_to_complete"]
 def mock_name(mock, name):
     type(mock).name = PropertyMock(return_value=name)
     return mock
+
+
+def set_localhost_transport_options(transport):
+    with open("/root/.ssh/id_rsa") as f:
+        transport["private-key"] = f.read()
+
+    transport["host-key"] = (
+        subprocess.check_output(["ssh-keyscan", "localhost"], encoding="utf8").split("\n")[-2].split(" ", 1)[1]
+    )
 
 
 def wait_replication_tasks_to_complete(zettarepl, timeout=300):
