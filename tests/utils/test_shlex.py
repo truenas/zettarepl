2aa1e5846076ecd6e9ca5123a67996f50e5e2fab
themylogin <themylogin@gmail.com>
2020-01-02 21:48:59 +0100

    Simulate shell pipefail

--- tests/utils/test_shlex.py
+++ tests/utils/test_shlex.py
@@ -1,9 +1,28 @@
 # -*- coding=utf-8 -*-
+import subprocess
+
+import pytest
+
 from zettarepl.utils.shlex import pipe
 
 
-def test__pipe():
-    assert (
-        pipe(["zfs", "send", "my dataset@snap"], ["zfs", "recv", "my'dataset"]) ==
-        ["sh", "-c", "zfs send 'my dataset@snap' | zfs recv 'my'\"'\"'dataset'"]
-    )
+@pytest.mark.parametrize("bad_command", [None] + list(range(4)))
+def test__pipe(bad_command):
+    commands = [
+        "echo a",
+        "sed 's/a/)/'",
+        "sed 's/)/\"/'",
+        "sed 's/\"/d/'",
+    ]
+    if bad_command is not None:
+        commands[bad_command] += f"; echo ERROR 1>&2; exit {bad_command * 10 + 1}"
+
+    piped = pipe(*[["sh", "-c", cmd] for cmd in commands])
+    print(piped[2])
+
+    cp = subprocess.run(piped, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+
+    assert cp.returncode == (0 if bad_command is None else bad_command * 10 + 1)
+    assert cp.stdout == "d\n"
+    if bad_command is not None:
+        assert cp.stderr == "ERROR\n"
