1e67e84adc0e6bffde17991d3bc2040d04138c33
themylogin <themylogin@gmail.com>
2020-01-11 14:45:42 +0100

    Fix replication not being stopped properly when killed by replication monitor

---
+++ integration-tests/transport/test_local_async_exec_stop.py
@@ -0,0 +1,27 @@
+# -*- coding=utf-8 -*-
+import subprocess
+import time
+
+from zettarepl.transport.local import LocalShell
+from zettarepl.utils.shlex import pipe
+
+
+def test__local_async_exec_stop():
+    def assert_marker_count(count):
+        assert int(subprocess.check_output(
+            "ps axw | grep ZETTAREPL_TEST_MARKER_1 | grep -v grep | wc -l", shell=True, encoding="utf-8",
+        ).strip()) == count
+
+    assert_marker_count(0)
+
+    local_shell = LocalShell()
+    exec = local_shell.exec_async(pipe(["python", "-c", "'ZETTAREPL_TEST_MARKER_1'; import time; time.sleep(60)"],
+                                       ["python", "-c", "'ZETTAREPL_TEST_MARKER_1'; import time; time.sleep(60)"]))
+
+    time.sleep(2)
+    assert_marker_count(6)
+
+    exec.stop()
+
+    time.sleep(1)
+    assert_marker_count(0)
