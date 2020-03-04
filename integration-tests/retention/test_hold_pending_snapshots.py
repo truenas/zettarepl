1b5c55cc2d088bf4aec676d342ffc854b1421a66
themylogin <themylogin@gmail.com>
2020-03-04 17:03:39 +0100

    test_hold_pending_snapshots__does_not_delete_orphan_snapshots

--- integration-tests/retention/test_hold_pending_snapshots.py
+++ integration-tests/retention/test_hold_pending_snapshots.py
@@ -71,3 +71,65 @@ def test_hold_pending_snapshots(hold_pending_snapshots, remains):
     zettarepl._run_local_retention(datetime(2018, 10, 1, 3, 0))
 
     assert list_snapshots(local_shell, "data/src", False) == remains
+
+
+def test_hold_pending_snapshots__does_not_delete_orphan_snapshots():
+    subprocess.call("zfs destroy -r data/src", shell=True)
+    subprocess.call("zfs destroy -r data/dst", shell=True)
+
+    subprocess.check_call("zfs create data/src", shell=True)
+    subprocess.check_call("zfs snapshot data/src@2018-10-01_00-00", shell=True)
+    subprocess.check_call("zfs snapshot data/src@2018-10-01_01-00", shell=True)
+    subprocess.check_call("zfs snapshot data/src@2018-10-01_02-00", shell=True)
+    subprocess.check_call("zfs snapshot data/src@2018-10-01_03-00", shell=True)
+    subprocess.check_call("zfs snapshot data/src@2018-10-02_00-00", shell=True)
+    subprocess.check_call("zfs snapshot data/src@2018-10-03_00-00", shell=True)
+
+    subprocess.check_call("zfs create data/dst", shell=True)
+    subprocess.check_call("zfs snapshot data/dst@2018-10-01_00-00", shell=True)
+    subprocess.check_call("zfs snapshot data/dst@2018-10-01_01-00", shell=True)
+    subprocess.check_call("zfs snapshot data/dst@2018-10-01_02-00", shell=True)
+    subprocess.check_call("zfs snapshot data/dst@2018-10-01_03-00", shell=True)
+    subprocess.check_call("zfs snapshot data/dst@2018-10-02_00-00", shell=True)
+    subprocess.check_call("zfs snapshot data/dst@2018-10-03_00-00", shell=True)
+
+    definition = Definition.from_data(yaml.safe_load(textwrap.dedent("""\
+        timezone: "UTC"
+
+        periodic-snapshot-tasks:
+          src:
+            dataset: data/src
+            recursive: true
+            lifetime: PT48H
+            naming-schema: "%Y-%m-%d_%H-%M"
+            schedule:
+              minute: "0"
+              hour: "0"
+
+        replication-tasks:
+          src:
+            direction: push
+            transport:
+              type: local
+            source-dataset: data/src
+            target-dataset: data/dst
+            recursive: true
+            periodic-snapshot-tasks:
+              - src
+            auto: true
+            retention-policy: source
+            hold-pending-snapshots: true
+    """)))
+
+    local_shell = LocalShell()
+    zettarepl = Zettarepl(Mock(), local_shell)
+    zettarepl.set_tasks(definition.tasks)
+    zettarepl._run_local_retention(datetime(2018, 10, 4, 0, 0))
+
+    assert list_snapshots(local_shell, "data/src", False) == [
+        Snapshot("data/src", "2018-10-01_01-00"),
+        Snapshot("data/src", "2018-10-01_02-00"),
+        Snapshot("data/src", "2018-10-01_03-00"),
+        Snapshot("data/src", "2018-10-02_00-00"),
+        Snapshot("data/src", "2018-10-03_00-00"),
+    ]
