51f858c70b4dab908f5422f9b2c95030fb13ed3a
themylogin <themylogin@gmail.com>
2021-08-23 20:39:47 +0200

    Check "no snapshots to send" for full replication, zfs send -R is not atomic and might fool us
    (cherry picked from commit 5010f145d337a1026cf6c8c1fb29eb5e9f940e2c)

---
+++ integration-tests/replication/test_replicate.py
@@ -0,0 +1,63 @@
+# -*- coding=utf-8 -*-
+import subprocess
+import textwrap
+
+import pytest
+import yaml
+
+from zettarepl.utils.test import run_replication_test
+
+
+@pytest.mark.parametrize("snapshot_to_destroy,error_text", [
+    # Fake incomplete `zfs send -R` by removing one of the children's most recent snapshots.
+    (
+        "data/dst/child2@2021-08-23_19-30",
+        (
+            f"Last full ZFS replication failed to transfer snapshot data/src@2021-08-23_19-30 as a whole. The snapshot "
+            "data/dst/child2@2021-08-23_19-30 was not transferred. Please run `zfs destroy data/dst@2021-08-23_19-30` "
+            "on the target system and run replication again."
+        ),
+    ),
+    # Older child snapshots might have been removed by retention or manually, we should not care about them.
+    ("data/dst/child2@2021-08-23_19-25", None),
+])
+@pytest.mark.parametrize("snapshot_match_options", [
+    {"also-include-naming-schema": ["%Y-%m-%d_%H-%M"]},
+    {"name-regex": ".+"},
+])
+def test_properties_exclude(snapshot_to_destroy, error_text, snapshot_match_options):
+    subprocess.call("zfs destroy -r data/src", shell=True)
+    subprocess.call("zfs receive -A data/dst", shell=True)
+    subprocess.call("zfs destroy -r data/dst", shell=True)
+
+    subprocess.check_call("zfs create data/src", shell=True)
+    subprocess.check_call("zfs create data/src/child1", shell=True)
+    subprocess.check_call("zfs create data/src/child2", shell=True)
+    subprocess.check_call("zfs snapshot -r data/src@2021-08-23_19-25", shell=True)
+    subprocess.check_call("zfs snapshot -r data/src@2021-08-23_19-30", shell=True)
+    subprocess.check_call("zfs send -R data/src@2021-08-23_19-25 | zfs recv data/dst", shell=True)
+    subprocess.check_call("zfs send -R -i data/src@2021-08-23_19-25 data/src@2021-08-23_19-30 | "
+                          "zfs recv data/dst", shell=True)
+    subprocess.check_call(f"zfs destroy {snapshot_to_destroy}", shell=True)
+
+    definition = yaml.safe_load(textwrap.dedent("""\
+        timezone: "UTC"
+
+        replication-tasks:
+          src:
+            direction: push
+            transport:
+              type: local
+            source-dataset: data/src
+            target-dataset: data/dst
+            recursive: true
+            replicate: true
+            auto: false
+            retention-policy: none
+            retries: 1
+    """))
+    definition["replication-tasks"]["src"].update(snapshot_match_options)
+
+    error = run_replication_test(definition, success=error_text is None)
+    if error_text is not None:
+        assert error.error == error_text
