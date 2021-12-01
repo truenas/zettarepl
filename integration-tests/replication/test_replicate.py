c0a36e6514b2c0acc4929e4e2f42fe59d440b880
David Durrleman <david.durrleman@shift-technology.com>
2021-12-01 14:17:37 +0100

    NAS-113577 / 22.02-RC.2 / Fix handling of partially replicated snapshots (#192)
    * Fix handling of partially replicated snapshots
    
    Cover cases where a new snapshot has been taken since the last failure,
    in which case most_recent_snapshot would not be the relevant one to
    look at. incremental_base on the other hand, is always relevant.
    Also in this case we need to run the check even if though 'snapshots'
    may not be equal to 'None' (because of the newly taken snapshot)
    
    Also add "-r" to the suggested command to run. Indeed, some children
    may have been replicated before the failure, and must be deleted too
    
    * Fix typo
    
    * Update error message in test
    
    * Add test case covering when a new snapshot has been taken
    
    * Remove obsolete optimization
    
    We always need to query createtxg in order to get an accurate value for
    incremental_base, which is required to check for inconsistent replication
    states due to a past replication that would have failed halfway.
    
    (cherry picked from commit 5e768d7011c93cb771622253e603a1493c4eb098)

--- integration-tests/replication/test_replicate.py
+++ integration-tests/replication/test_replicate.py
@@ -15,13 +15,14 @@ from zettarepl.utils.test import run_replication_test
         (
             f"Last full ZFS replication failed to transfer all the children of the snapshot data/src@2021-08-23_19-30. "
             "The snapshot data/dst/child2@2021-08-23_19-30 was not transferred. Please run "
-            "`zfs destroy data/dst@2021-08-23_19-30` on the target system and run replication again."
+            "`zfs destroy -r data/dst@2021-08-23_19-30` on the target system and run replication again."
         ),
     ),
     # Older child snapshots might have been removed by retention or manually, we should not care about them.
     ("data/dst/child2@2021-08-23_19-25", None),
 ])
-def test_properties_exclude(snapshot_to_destroy, error_text):
+@pytest.mark.parametrize("take_new_snapshot", [True, False])
+def test_properties_exclude(snapshot_to_destroy, error_text, take_new_snapshot):
     subprocess.call("zfs destroy -r data/src", shell=True)
     subprocess.call("zfs receive -A data/dst", shell=True)
     subprocess.call("zfs destroy -r data/dst", shell=True)
@@ -35,6 +36,9 @@ def test_properties_exclude(snapshot_to_destroy, error_text):
     subprocess.check_call("zfs send -R -i data/src@2021-08-23_19-25 data/src@2021-08-23_19-30 | "
                           "zfs recv data/dst", shell=True)
     subprocess.check_call(f"zfs destroy {snapshot_to_destroy}", shell=True)
+
+    if take_new_snapshot:
+        subprocess.check_call(f"zfs snapshot -r data/src@2021-08-23_19-35", shell=True)
 
     definition = yaml.safe_load(textwrap.dedent("""\
         timezone: "UTC"
