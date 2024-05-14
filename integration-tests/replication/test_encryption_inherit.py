c49cfb84ae1f8773d8fc3b425fa496c61e12758a
themylogin <themylogin@gmail.com>
2024-05-14 20:34:58 +0200

    Fix `existing_parent_is_encrypted`

--- integration-tests/replication/test_encryption_inherit.py
+++ integration-tests/replication/test_encryption_inherit.py
@@ -2,10 +2,9 @@
 import subprocess
 import textwrap
 
-import pytest
 import yaml
 
-from zettarepl.utils.test import create_dataset, run_replication_test, transports
+from zettarepl.utils.test import create_dataset, run_replication_test
 
 
 def test_inherit_encryption_when_parent_is_not_encrypted():
@@ -41,3 +40,35 @@ def test_inherit_encryption_when_parent_is_not_encrypted():
         "Encryption inheritance requested for destination dataset 'data/dst/child/grandchild', but its existing parent "
         "is not encrypted."
     )
+
+
+def test_inherit_encryption_when_parent_is_encrypted():
+    subprocess.call("zfs destroy -r data/src", shell=True)
+    subprocess.call("zfs destroy -r data/dst", shell=True)
+
+    create_dataset("data/src")
+    subprocess.check_call("zfs snapshot -r data/src@2018-10-01_01-00", shell=True)
+
+    create_dataset("data/dst")
+    create_dataset("data/dst/child", encrypted=True)
+
+    definition = yaml.safe_load(textwrap.dedent("""\
+        timezone: "UTC"
+
+        replication-tasks:
+          src:
+            direction: push
+            transport:
+                type: local
+            source-dataset: data/src
+            target-dataset: data/dst/child/grandchild
+            recursive: false
+            encryption: inherit
+            also-include-naming-schema:
+              - "%Y-%m-%d_%H-%M"
+            auto: false
+            retention-policy: none
+            retries: 1
+    """))
+
+    run_replication_test(definition)
