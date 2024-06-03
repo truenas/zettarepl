f9a785cca8be77d318531cabff84930fef8ed71c
themylogin <themylogin@gmail.com>
2024-06-03 16:15:53 +0200

    Use `truenas_api_client`

--- zettarepl/truenas/removal_dates.py
+++ zettarepl/truenas/removal_dates.py
@@ -7,6 +7,6 @@ __all__ = ["get_removal_dates"]
 
 
 def get_removal_dates():
-    from middlewared.client import Client
+    from truenas_api_client import Client
     with Client() as c:
         return c.call("zettarepl.get_removal_dates")
